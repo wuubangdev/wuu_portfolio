7790b0a276c29a2a6dbb5e9d3b0d8caef8f10d8b
wuubangdev <wuubangdev@gmail.com>
2025-04-21 18:17:36 +0700

    skill

--- src/components/skills/SkillItem.tsx
+++ src/components/skills/SkillItem.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import "./skill.css"
 const SkillItem = () => {
     return (
-        <div className="container">
-            <div data-text="Github" className="glass w-full">
+        <div className="container group">
+            <div className="glass w-full">
+                <div className='w-full h-full relative bg-amber-300 group-hover:bg-amber-500 transition-all duration-300 ease-in-out'>
+
+                </div>
+                <div className='px-4 py-2'>GIT</div>
             </div>
         </div>
 
