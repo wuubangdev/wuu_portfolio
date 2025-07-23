05baa93c8f74e0c7ff4e310f2a4a267b5a153522
wuubangdev <wuubangdev@gmail.com>
2025-07-23 23:12:10 +0700

    add s3 media

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,7 +1,7 @@
 'use client'
-import React, { useEffect } from 'react'
+import React from 'react'
 import HeaderSection from '../utils/HeaderSection';
-import { Project } from '@/types';
+import { IProjectDetail, Project } from '@/types';
 import ProjectItem from './ProjectItem';
 
 
@@ -16,6 +16,19 @@ const projectsData: Project[] = [
         description: "Full Stack Web Developer",
         imageUrl: "/project/thingtodo.png",
         linkGit: "",
+        detail: {
+            title: "Thing to do home page",
+            description: "A simple home page for a to-do list application, showcasing a clean and user-friendly interface.",
+            imageUrl: ["https://wuubang-access.s3.ap-southeast-1.amazonaws.com/ttd.mp4"],
+            linkLive: "https://thingtodo.vn/",
+            category: "Website",
+            technologies: ["Java", "TypeScript"],
+            libraries: {
+                frontend: ["React", "Nextjs", "Tailwind CSS", "Antdesign", "Framer Motion"],
+                backend: ["Spring 3.0", "MySQL", "TypeORM", "Restful API"],
+                other: ["AWS S3", "Nginx", "Docker", "GitHub Actions"]
+            }
+        },
     },
     {
         id: 2,
@@ -23,6 +36,19 @@ const projectsData: Project[] = [
         description: "Frontend Web Developer",
         imageUrl: "/project/kem.png",
         linkGit: "",
+        detail: {
+            title: "Icream project landing page",
+            description: "A simple home page for a to-do list application, showcasing a clean and user-friendly interface.",
+            imageUrl: ["https://wuubang-access.s3.ap-southeast-1.amazonaws.com/icrem.mp4"],
+            linkLive: "https://kemcalem.thingtodo.vn/",
+            category: "Website",
+            technologies: ["TypeScript"],
+            libraries: {
+                frontend: ["Nextjs", "Tailwind CSS", "Slick Carousel"],
+                backend: ["No backend"],
+                other: ["AWS S3", "Nginx"]
+            }
+        },
     },
     {
         id: 3,
@@ -30,6 +56,19 @@ const projectsData: Project[] = [
         description: "Full Stack Web Developer",
         imageUrl: "/project/diemmy.png",
         linkGit: "",
+        detail: {
+            title: "Luxury beauty spa",
+            description: "A simple home page for a to-do list application, showcasing a clean and user-friendly interface.",
+            imageUrl: ["https://wuubang-access.s3.ap-southeast-1.amazonaws.com/diemmy.mp4"],
+            linkLive: "https://diemmy.bangdev.click/",
+            category: "Website",
+            technologies: ["React", "Tailwind CSS", "TypeScript"],
+            libraries: {
+                frontend: ["Nextjs", "React", "Tailwind CSS", "antdesign", "Slick Carousel"],
+                backend: ["Spring 3.0", "MySQL", "TypeORM", "Restful API"],
+                other: ["Axios", "Nginx", "Docker", "GitHub Actions", "AWS S3"]
+            }
+        },
     },
     {
         id: 4,
@@ -37,6 +76,19 @@ const projectsData: Project[] = [
         description: "Full Stack Web Developer",
         imageUrl: "/project/lrd.png",
         linkGit: "",
+        detail: {
+            title: "Conference website",
+            description: "A simple home page for a to-do list application, showcasing a clean and user-friendly interface.",
+            imageUrl: ["https://wuubang-access.s3.ap-southeast-1.amazonaws.com/lrd.mp4"],
+            linkLive: "https://lrd-conf.bangdev.click/",
+            category: "Website",
+            technologies: ["Java", "TypeScript"],
+            libraries: {
+                frontend: ["Nextjs", "Tailwind CSS"],
+                backend: ["Nestjs", "MongoDB", "Restful API"],
+                other: ["Axios", "Nginx", "Docker", "GitHub Actions", "AWS S3"]
+            }
+        },
     },
     {
         id: 5,
@@ -44,6 +96,19 @@ const projectsData: Project[] = [
         description: "Full Stack Web Developer",
         imageUrl: "/project/paulthien.png",
         linkGit: "",
+        detail: {
+            title: "Portfolio website",
+            description: "A simple home page for a to-do list application, showcasing a clean and user-friendly interface.",
+            imageUrl: ["https://wuubang-access.s3.ap-southeast-1.amazonaws.com/paul.mp4"],
+            linkLive: "https://www.paulthien97.com/",
+            category: "Website",
+            technologies: ["TypeScript"],
+            libraries: {
+                frontend: ["React", "Tailwind CSS"],
+                backend: ["Nestjs", "MySQL", "Restful API"],
+                other: ["Axios", "Nginx", "Docker", "GitHub Actions", "AWS S3"]
+            }
+        },
     },
     {
         id: 6,
@@ -51,29 +116,29 @@ const projectsData: Project[] = [
         description: "Frontend Web Developer",
         imageUrl: "/project/3d.png",
         linkGit: "",
+        detail: {
+            title: "3D viewer page",
+            description: "A simple home page for a to-do list application, showcasing a clean and user-friendly interface.",
+            imageUrl: ["https://wuubang-access.s3.ap-southeast-1.amazonaws.com/3d.mp4"],
+            linkLive: "https://ttd-3-d-viewer.vercel.app/",
+            category: "Website",
+            technologies: ["React"],
+            libraries: {
+                frontend: ["React", "Tailwind CSS", "Three.js"],
+                backend: ["No backend"],
+                other: ["Nginx",]
+            }
+        },
     },
 ]
 
 const Projects: React.FC<ProjectProps> = ({ }) => {
-
-    const [projects, setProjects] = React.useState<Project[]>([]);
-    const [showMore, setShowMore] = React.useState(false);
-
-    useEffect(() => {
-        if (showMore) {
-            setProjects(projectsData);
-        }
-        else {
-            const result: Project[] = [];
-            projectsData.forEach((project, i) => {
-                if (i < 3) {
-                    result.push(project);
-                }
-            });
-            setProjects(result);
-        }
-    }, [showMore]);
-
+    const [showDetail, setShowDetail] = React.useState<boolean>(false);
+    const [detail, setDetail] = React.useState<IProjectDetail | null>(null);
+    const openDetailHandle = (project: Project) => {
+        setShowDetail(true);
+        setDetail(project.detail || null);
+    }
     return (
         <section className='w-full flex border-b-[1px] border-white/15 justify-center px-28 relative'>
             <div className='w-full flex flex-col gap-6 border-x-[1px] border-white/15 lg:py-28 lg:px-20 md:py-20 md:px-10 sm:py-10 sm:px-5'>
@@ -81,19 +146,68 @@ const Projects: React.FC<ProjectProps> = ({ }) => {
                     <HeaderSection title='Projects' />
                 </div>
                 <h1 className='text-center text-[4rem] font-medium leading-none cursor-default'>My Latest Project</h1>
-                <div className='pt-10 grid grid-cols-3 gap-6'>
-                    {projects && projects.map((project) => <ProjectItem key={`project-${project.id}`} project={project} />)}
+                <div className='pt-10 grid grid-cols-3 gap-6' >
+                    {projectsData && projectsData.map((project) => <div key={`project-${project.id}`} onClick={() => openDetailHandle(project)}>
+                        <ProjectItem project={project} />
+                    </div>)}
                 </div>
-                {!showMore &&
-                    <div className='p-2 flex items-center justify-center'>
-                        <button
-                            className='py-4 px-11 bg-gray-600 cursor-pointer hover:bg-amber-600 duration-500 text-lg font-medium'
-                            onClick={() => setShowMore(!showMore)}
-                        >
-                            View All Project
-                        </button>
-                    </div>}
             </div>
+            {showDetail && (
+                <div className='fixed inset-0 bg-black/80 flex items-center justify-center z-99999'>
+                    <div className="mx-auto w-4/5 p-6 bg-gray-900 shadow-lg relative">
+                        <div className='flex items-end gap-3 mb-4'>
+                            <h1 className="text-2xl font-bold text-white uppercase">{detail?.title}</h1>
+                            <p className="text-sm text-amber-700 font-medium italic bg-amber-100 px-2 rounded-lg">{detail?.category}</p>
+                        </div>
+                        <a
+                            href={detail?.linkLive}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-blue-600 hover:underline font-medium"
+                        >
+                            🔗 Visit Live Site
+                        </a>
+                        <div className='flex items-center justify-center mt-4 w-full'>
+                            <div className="aspect-video w-3/4 overflow-hidden rounded-lg">
+                                <video
+                                    controls
+                                    width="100%"
+                                    preload="metadata"
+                                >
+                                    <source src={detail?.imageUrl[0]} type="video/mp4" />
+                                </video>
+
+                            </div>
+                        </div>
+                        {/* <p className="text-gray-700">{detail?.description}</p> */}
+
+                        <div className="space-y-2 flex justify-between">
+                            <div className='flex gap-2'>
+                                <strong className="block font-semibold text-amber-600">Frontend:</strong>
+                                <p className=''>{detail?.libraries.frontend.join(', ')}</p>
+                            </div>
+                            <div className='flex gap-2'>
+                                <strong className="block font-semibold text-amber-600">Backend:</strong>
+                                <p className=''>{detail?.libraries.backend.join(', ')}</p>
+                            </div>
+                            {detail?.libraries.other && (
+                                <div className='flex gap-2'>
+                                    <strong className="block font-semibold text-amber-600">Other Tools:</strong>
+                                    <p className=''>{detail?.libraries.other.join(', ')}</p>
+                                </div>
+                            )}
+                            <button
+                                className='py-2 px-4 bg-gray-500 text-white hover:bg-gray-700 absolute top-4 right-4 transition duration-300 cursor-pointer'
+                                onClick={() => setShowDetail(false)}
+                            >
+                                X
+                            </button>
+                        </div>
+                        {/* Close modal */}
+
+                    </div>
+                </div>
+            )}
         </section>
     )
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -4,6 +4,7 @@ export interface Project {
     imageUrl: string;
     description: string;
     linkGit: string;
+    detail?: IProjectDetail;
 }
 
 export interface IService {
@@ -12,6 +13,20 @@ export interface IService {
     imageUrl: string;
     description: string;
     linkGit: string;
+}
+
+export interface IProjectDetail {
+    title: string;                     // Tiêu đề dự án
+    description: string;               // Mô tả chi tiết
+    imageUrl: string[];                // Danh sách link ảnh minh họa
+    linkLive: string;                  // Link trang web live/demo
+    category: string;                  // Phân loại dự án (VD: Website, Mobile App, Landing Page...)
+    technologies: string[];            // Các công nghệ chính sử dụng (VD: React, NestJS, MongoDB...)
+    libraries: {
+        frontend: string[];              // Các thư viện frontend (VD: Tailwind, Axios, Framer Motion...)
+        backend: string[];               // Các thư viện backend (VD: Express, TypeORM, Prisma...)
+        other?: string[];                // Các thư viện khác (VD: Socket.io, Stripe, Firebase...)
+    };
 }
 
 export interface IExperience {
