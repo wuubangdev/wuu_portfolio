3ef9a3c9de53d62e1cbeace5da1789f2619aaf80
wuubangdev <wuubangdev@gmail.com>
2025-07-24 11:19:59 +0700

    mobile about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      {/* <About /> */}
+      <About />
       {/* <Services /> */}
       {/* <Projects /> */}
       {/* <Skills /> */}
--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image';
 
 const About: React.FC = () => {
     return (
-        <section className='w-full flex border-b-[1px] border-white/15 justify-center px-28'>
-            <div className='w-full flex flex-col gap-6 border-x-[1px] border-white/15 lg:py-32 lg:px-20 md:py-20 md:px-10 sm:py-10 sm:px-5'>
+        <section className='w-full flex border-b-[1px] border-white/15 justify-center md:px-28 px-4'>
+            <div className='w-full flex flex-col gap-6 border-x-[1px] border-white/15 p-8 lg:py-32 lg:px-20 md:py-20 md:px-10 sm:py-10 sm:px-5'>
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-16 min-h-[100vh]'>
-                    <div className='w-full'>
+                    <div className='md:block hidden w-full'>
                         <div className='w-full sticky top-28 flex items-center justify-start'>
                             <div className='w-4/5 aspect-[4/5] mb-8 relative bg-amber-800'>
                                 <Image
@@ -30,29 +30,29 @@ const About: React.FC = () => {
                         <div className='w-full flex items-center justify-start'>
                             <HeaderSection title='About me' />
                         </div>
-                        <h1 className='text-[4rem] font-medium leading-none'>I Am Bang, Fullstack Developer</h1>
-                        <h5 className='text-3xl font-semibold text-[#a9a9a9]'>I have 8+ years of experiences in Software Development for give you better services.</h5>
+                        <h1 className='md:text-[4rem] text-[3rem] font-medium leading-none'>I Am Bang, Fullstack Developer</h1>
+                        <h5 className='md:text-3xl text-2xl font-semibold text-[#a9a9a9]'>I have 8+ years of experiences in Software Development for give you better services.</h5>
                         <p className='text-xl font-extralight text-[#919195]'>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite </p>
-                        <div className='pt-8 grid grid-cols-2 gap-10'>
-                            <div className='col-span-2 flex flex-col gap-2 cursor-pointer'>
-                                <div className='text-5xl text-amber-600 font-medium'>Can Tho University</div>
+                        <div className='md:pt-8 grid grid-cols-2 md:gap-10 gap-5'>
+                            <div className='col-span-2 flex flex-col gap-1 cursor-pointer'>
+                                <div className='text-3xl md:text-5xl text-amber-600 font-medium'>Can Tho University</div>
                                 <div className='text-[1rem] text-[#919195]'>Education</div>
                             </div>
-                            <div className='flex flex-col gap-2 cursor-pointer'>
-                                <div className='text-5xl text-amber-600 font-medium'>Master</div>
+                            <div className='flex flex-col gap-1 cursor-pointer'>
+                                <div className='text-3xl md:text-5xl text-amber-600 font-medium'>Master</div>
                                 <div className='text-[1rem] text-[#919195]'>Degree</div>
                             </div>
-                            <div className='flex flex-col gap-2 cursor-pointer'>
-                                <div className='text-5xl text-amber-600 font-medium'>3.6</div>
+                            <div className='flex flex-col gap-1 cursor-pointer'>
+                                <div className='text-3xl md:text-5xl text-amber-600 font-medium'>3.6</div>
                                 <div className='text-[1rem] text-[#919195]'>GPA</div>
                             </div>
 
-                            <div className='flex flex-col gap-2 cursor-pointer'>
-                                <div className='text-5xl text-amber-600 font-medium'>500+</div>
+                            <div className='flex flex-col gap-1 cursor-pointer'>
+                                <div className='text-3xl md:text-5xl text-amber-600 font-medium'>500+</div>
                                 <div className='text-[1rem] text-[#919195]'>Projects Completed</div>
                             </div>
-                            <div className='flex flex-col gap-2 cursor-pointer'>
-                                <div className='text-5xl text-amber-600 font-medium'>HCM, VN</div>
+                            <div className='flex flex-col gap-1 cursor-pointer'>
+                                <div className='text-3xl md:text-5xl text-amber-600 font-medium'>HCM, VN</div>
                                 <div className='text-[1rem] text-[#919195]'>Address</div>
                             </div>
                         </div>
